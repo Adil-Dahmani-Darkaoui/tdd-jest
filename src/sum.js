1c125ffbe69db000b517402664d1b4b7a0e94de2
Adil D.Dark <adil.dahmani-darkaoui@le-campus-numerique.fr>
2020-08-26 16:04:58 +0200

    test

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,17 @@
 // Implement the sum function here
+function sum(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    } else {
+        console.log("Error");
+    }
+}
+module.exports = sum;
+
+function multiplyBy()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+module.exports = multiplyBy();
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,20 @@
 // Add the unit tests of the sum function here
+const sum = require('../src/sum');
+const multiplyBy = require('../src/sum');
+const a = 1;
+const b = 2;
+const x = "x";
+
+
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+    test('adds 1 + 2 to equal 3', () => {
+        expect(() => {
+            sum(1, x).toThrow()
+        })
+    });
+
+test('multiply num1 by num2', () => {
+    expect(multiplyBy())
+})
